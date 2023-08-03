6bab69fce1189df9b4b75d11f8e5628e08ba7e60
Lori Haffelt <ljhcnmt@gmail.com>
2023-08-03 19:04:43 -0400

    typedefs, mutations, etc

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,23 +2,37 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
-    _id: ID!
-    name: String!
+    _id: ID
+    username: String
+    email: String
+    password: String
+    games: [Game]!
   }
 
   type Game {
-    _id: ID!
-    name: String!
+    _id: ID
+    gameData: Object
+    createdAt: String
+  }
+
+
+  type Auth {
+    token: ID!
+    user: User
   }
 
   type Query {
-    user: [User]
-    game: (_id: String): [Game]
+    user(username: String!): User
+    games(username: String): [Game]
+    game(gameId: ID!): Game
+    me: User
   }
 
   type Mutation {
-    createUser(name: String!): User
-    createGame(_id: String!, name: String): Game
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addGame(gameData: String!): Game
+    removeGame(gameId: ID!): Game
   }
 `;
 
