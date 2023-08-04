f7f5d2ac4e5564a63bf0f287c337f42916a7d9fc
Lori Haffelt <ljhcnmt@gmail.com>
2023-08-03 21:53:04 -0400

    login working

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,11 +1,31 @@
-const auth = (req, res, next) => {
-    console.log(req.session.loggedIn)
-      // redirect the request to the login 
-      if (!req.session.loggedIn) {
-        res.redirect('/login');
-      } else {
-        next();
-      }
-    };
-    
-    module.exports = auth;
+const jwt = require('jsonwebtoken');
+
+const secret = 'mysecretssshhhhhhh';
+const expiration = '8h';
+
+module.exports = {
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+  signToken: function ({ email, username, _id }) {
+    const payload = { email, username, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
