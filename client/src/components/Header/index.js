a9638a382ec6e5f43bd48ccf3d99d0f391e20c33
Andres Rodriguez <84158011+andyr-dev@users.noreply.github.com>
2023-08-03 20:50:20 -0400

    updated header and nav

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,39 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
-import Auth from "../../utils/auth";
-
+import logo from "../../assets/Logo.png";
 
 // update Header info
 
-
-const Header = () => {
-  const logout = (event) => {
-    event.preventDefault();
-    Auth.logout();
-  
-};
-return (
-  <header>
-  <div className="header">
-  <Link to="/" className="logo-link">
-    <h1 className="logo-text">Log In</h1>
-  </Link>
-</div>
-<div className="nav-container">
-  {Auth.loggedIn() ? (
-    <>
-      <Link to="/me" className="nav-link">{Auth.getProfile().data.username}'s profile</Link>
-      <button onClick={logout} className="logout-button">Logout</button>
-    </>
-  ) : (
-    <>
-      <Link to="/login" className="nav-link">Login</Link>
-      <Link to="/signup" className="nav-link">Signup</Link>
-    </>
-  )}
-</div>
-</header>
-);
+export default function Header() {
+  return (
+    <header>
+      <Link to="/">
+        <img src={logo} alt="TeeTrackerLogo" className="logo" />
+      </Link>
+    </header>
+  );
 }
-export default Header;
