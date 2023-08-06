eaf5b19d8e21553151c53caf693f7cb1a5cad1aa
LaiSha Buck <laishabuck@gmail.com>
2023-08-05 20:09:21 -0400

    added styling to the scorecard

--- client/src/pages/ScoreCard.js
+++ client/src/pages/ScoreCard.js
@@ -28,56 +28,62 @@ const ScoreCard = () => {
       return newCounts;
     });
   };
-
   return (
-    <div className= "scorecard-container">
-       <h1 className="scorecard-title">Golf Score Card</h1>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Hole</TableCell>
-              <TableCell>Par</TableCell>
-              <TableCell>F.I.R</TableCell>
-              <TableCell>G.I.R</TableCell>
-              <TableCell>Putts</TableCell>
-              <TableCell>Score to Par</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {holeCounts.map((count, index) => (
-              <TableRow key={index + 1}>
-                <TableCell>{index + 1}</TableCell>
-                <TableCell className="par-input" contentEditable="true"></TableCell>
-                <TableCell>
-                  <input type="checkbox" />
-                </TableCell>
-                <TableCell>
-                  <input type="checkbox" />
-                </TableCell>
-                <TableCell>
-                  <Button
-                    variant="outlined"
-                    onClick={() => handleIncrement(index)}
-                  >
-                    -
-                  </Button>
-                  <span>{count}</span>
-                  <Button
-                    variant="outlined"
-                    onClick={() => handleDecrement(index)}
-                  >
-                    +
-                  </Button>
-                </TableCell>
-                <TableCell className="par-input" contentEditable="true"></TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
-  );
+  <div className="scorecard-container">
+  <h1 className="scorecard-title">Golf Score Card</h1>
+ <TableContainer component={Paper}>
+   <Table>
+     <TableHead>
+       <TableRow>
+         <TableCell>Hole</TableCell>
+         <TableCell>Par</TableCell>
+         <TableCell>F.I.R</TableCell>
+         <TableCell>G.I.R</TableCell>
+         <TableCell>Putts</TableCell>
+         <TableCell>Score to Par</TableCell>
+       </TableRow>
+     </TableHead>
+     <TableBody>
+       {holeCounts.map((count, index) => (
+         <TableRow key={index + 1} className="table-row">
+           <TableCell>{index + 1}</TableCell>
+           <TableCell className="par-input" contentEditable="true"></TableCell>
+           <TableCell>
+             <input type="checkbox" className="checkbox-input" />
+           </TableCell>
+           <TableCell>
+             <input type="checkbox" className="checkbox-input" />
+           </TableCell>
+           <TableCell>
+             <Button
+               variant="outlined"
+               onClick={() => handleIncrement(index)}
+               className="score-button"
+             >
+               -
+             </Button>
+             <span className="score-count">{count}</span>
+             <Button
+               variant="outlined"
+               onClick={() => handleDecrement(index)}
+               className="score-button"
+             >
+               +
+             </Button>
+           </TableCell>
+           <TableCell className="par-input" contentEditable="true"></TableCell>
+         </TableRow>
+       ))}
+     </TableBody>
+   </Table>
+ </TableContainer>
+</div>
+);
 };
 
 export default ScoreCard;
+
+
+
+
+
